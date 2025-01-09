bb6ad81f970ba7c64cf10c4758e64f98d6df6da8
Prajwal Khadgi <prajwalkhadgi11@gmail.com>
2025-01-09 21:57:37 +0545

    feat: add form

--- src/components/Exams/ExamSection/ExamSection.tsx
+++ src/components/Exams/ExamSection/ExamSection.tsx
@@ -1,12 +1,12 @@
-import ExamCard from "../ExamCard";
+import Icon from "@Components/common/Icon";
 
 export default function ExamSection() {
   return (
     <section className="naxatw-h-full naxatw-p-4">
-      <div className="naxatw-h-full">
-        <h6>These are the currently available exams:</h6>
-        <div className="naxatw-mt-6">
-          <ExamCard />
+      <div className="naxatw-h-[80%] naxatw-flex naxatw-items-center naxatw-justify-center">
+        <div className="naxatw-flex naxatw-flex-col naxatw-items-center naxatw-gap-2">
+          <Icon name="delete_forever" className="naxatw-text-[10em]" />
+          <h6>Exams not available at the moment go to form section</h6>
         </div>
       </div>
     </section>
--- src/components/Exams/FormSection/index.tsx
+++ src/components/Exams/FormSection/index.tsx
@@ -3,11 +3,20 @@ import Icon from "@Components/common/Icon";
 export default function FormSection() {
   return (
     <section className="naxatw-h-full naxatw-w-full naxatw-p-4">
-      <div className="naxatw-h-[80%] naxatw-flex naxatw-items-center naxatw-justify-center">
-        <div className="naxatw-flex naxatw-flex-col naxatw-items-center naxatw-gap-2">
-          <Icon name="delete_forever" className="naxatw-text-[10em]" />
-          <h6>No Forms Available at the moment</h6>
-        </div>
+      <div
+        className="naxatw-w-[20rem] naxatw-cursor-pointer naxatw-border-primary naxatw-flex naxatw-h-[4rem] naxatw-gap-2 naxatw-justify-center naxatw-items-center naxatw-p-4 naxatw-border naxatw-rounded-md"
+        onClick={() =>
+          window.open(
+            "https://docs.google.com/forms/d/e/1FAIpQLSdXQT4kRARLykvhLpgBpcbl-th53jy3FhIxNuOiXyMrzzQJ0g/viewform?usp=send_form",
+            "_blank",
+          )
+        }
+      >
+        <Icon
+          name="summarize"
+          className="naxatw-text-[10em] naxatw-text-primary"
+        />
+        <h5>Geomatics License Exam Form</h5>
       </div>
     </section>
   );
